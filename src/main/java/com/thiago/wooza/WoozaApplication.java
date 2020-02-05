4422791d24a12a024a4b77222022ad6b31ffafaa
thiago.siqueira <thiago.siqueira@accenture.com>
2020-02-05 17:54:36 -0300

    Criado repositorio e projeto

---
+++ src/main/java/com/thiago/wooza/WoozaApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.wooza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoozaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoozaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/wooza/WoozaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.wooza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoozaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
